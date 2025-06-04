58733214c7da4bce1d4c158e59591010a25c28b5
Anupguru <anup52398@gmail.com>
2025-06-05 01:16:31 +0600

    projects

--- models/index.js
+++ models/index.js
@@ -2,19 +2,9 @@ const User = require('./userModel');
 const Student = require('./studentModel');
 const Attendance = require('./attendanceModel');
 
-// Associations
-User.hasMany(Attendance, {
-  foreignKey: 'submitted_by',
-  as: 'submitted_attendances',
-  onDelete: 'SET NULL',
-  onUpdate: 'CASCADE'
-});
-
-Student.hasMany(Attendance, {
-  foreignKey: 'studentId',
-  as: 'attendances',
-  onDelete: 'CASCADE'
-});
+User.hasMany(Attendance, { foreignKey: 'submitted_by', as: 'submitted_attendances' });
+Student.hasMany(Attendance, { foreignKey: 'studentId', as: 'attendances' });
+Attendance.belongsTo(Student, { foreignKey: 'studentId', as: 'student' }); 
 
 // Export all models
 module.exports = {
